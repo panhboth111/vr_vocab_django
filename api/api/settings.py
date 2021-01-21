17c8c9738c40caa4c141c8a11f2ff24d3bfaf061
sokhomsovichea <sokhomsovichea18@kit.edu.kh>
2021-01-21 14:54:59 +0700

    dockerize

--- api/api/settings.py
+++ api/api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6a0h76$tgz6a8*yss+hz3e^kxwai^+olx%x8af!66nn$uh7-k='
+# SECRET_KEY = '6a0h76$tgz6a8*yss+hz3e^kxwai^+olx%x8af!66nn$uh7-k='
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
 
 
 # Application definition
